b5eea8dc96d3a555223d9fffdbed10e945e54c64
Ayman-Guaiche <guaicheayman@gmail.com>
2024-01-26 17:41:57 +0000

    create product design

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-
+handleCheckProduct(product: any) {
+    product.checked = !product.checked;
+}
+  products : Array<any> = [
+    {id : 1, name : "computer" , price : 5000 , checked: false},
+    {id : 2, name : "printer" , price : 6000 , checked: true},
+    {id : 3, name : "phone" , price : 2000 , checked: false}
+  ]
 }
